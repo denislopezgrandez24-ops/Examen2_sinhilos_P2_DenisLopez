c8cdcbdffc307a885440662ccf82c56624f196da
denislopezgrandez24-ops <denislopezgrandez24@gmail.com>
2026-09-24 17:18:34 -0600

    primer commit

---
+++ src/examen2_sinhilos_p2_denislopez/Examen2_sinhilos_P2_DenisLopez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2_sinhilos_p2_denislopez;
+
+/**
+ *
+ * @author denis
+ */
+public class Examen2_sinhilos_P2_DenisLopez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
